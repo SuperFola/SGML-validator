eea52ccdce55de79432934a669a82a69ee159d73
Alexandre Plateau <lexplt.dev@gmail.com>
2020-02-13 09:18:13 +0100

    definition of stack_next

--- include/kafe/stack.h
+++ include/kafe/stack.h
@@ -14,6 +14,7 @@ int stack_init(Stack *s);
 int stack_free(Stack *s);
 
 int stack_push(Stack *s, char c);
+int stack_next(Stack *s);
 int is_stack_empty(Stack *s);
 int stack_cmp_top(Stack *s, const char *value);
 int stack_pop(Stack *s);
